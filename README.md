bec48ffc42a6825951d3d40e05abec6fe9be01ac
Dan Young <danielmyoung@gmail.com>
2016-10-01 13:32:42 -0700

    Add script; vim-plug and plugins

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # dotfiles
 
 These are dotfiles. There are many like them, but these ones are mine.
+
+## Usage
+
+```sh
+cd ~
+git clone git@github.com:idleyoungman/dotfiles.git
+cd dotfiles
+./doit.sh
+```
---
+++ doit.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -e
+
+# is stow in our $PATH?
+command -v stow
+
+# run stow against all dirs
+for dir in *; do
+  if [ -d "$dir" ]; then
+    stow --verbose=2 "$dir"
+  fi
+done
+
+# install vim-plug
+if [ ! -f ~/.vim/autoload/plug.vim ]; then
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+# update vim-plug
+vim +PlugUpgrade +qall
+
+# install and update vim plugins
+vim +PlugInstall +PlugUpdate +qall
